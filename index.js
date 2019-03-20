4701f50f35b9fedc7376ef515167a50f536ac7de
Chance Hudson <jchancehud@gmail.com>
2019-03-20 07:03:28 -0500

    Add express, mongoose, basic server with db connection

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-module.exports = (req, res) => {
-  res.end('Hello world');
-};
+const mongoose = require('mongoose');
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const app = express();
+
+app.use(express.json());
+
+/**
+ * Establish a connection to the mongo database, then continue the request
+ **/
+app.use(
+  asyncHandler(async (req, res, next) => {
+    await mongoose.connect(process.env.DB_URI, {
+      connectTimeoutMS: 5000,
+      useNewUrlParser: true,
+    });
+    next();
+  })
+);
+
+app.use((req, res) => res.end('Hello World'));
+
+module.exports = app;
