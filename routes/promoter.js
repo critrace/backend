de3a2922cca687eebe0a53f4353d85597d7c5dc7
Chance Hudson <jchancehud@gmail.com>
2019-03-27 02:34:33 -0500

    Bugfixes, improved error handling

--- routes/promoter.js
+++ routes/promoter.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Promoter = mongoose.model('Promoter');
 const _async = require('async-express');
 const emailValidator = require('email-validator');
+const bcrypt = require('bcrypt');
 
 module.exports = (app) => {
   app.post('/promoter', create);
@@ -10,18 +11,20 @@ module.exports = (app) => {
 
 const create = _async(async (req, res) => {
   if (!req.body.password || req.body.password.length < 6) {
-    res.status(400);
-    res.send('Please make sure your password is at least 6 characters.');
+    res.status(400).json({
+      message: 'Please make sure your password is at least 6 characters.',
+    });
     return;
   }
   if (!emailValidator.validate(req.body.email)) {
-    res.status(400);
-    res.send('Invalid email supplied.');
+    res.status(400).json({
+      message: 'Invalid email supplied.',
+    });
     return;
   }
   const salt = await bcrypt.genSalt(10);
   const passwordHash = await bcrypt.hash(req.body.password, salt);
-  const created = await Promoter.create({ ...req.body, passwordHash });
+  const created = await Promoter.create({ ...req.body, passwordHash, createdAt: new Date() });
   res.json(created);
 });
 
@@ -30,14 +33,16 @@ const login = _async(async (req, res) => {
     email: req.body.email
   }).lean().exec();
   if (!promoter) {
-    res.status(400);
-    res.send('Promoter email not registered.');
+    res.status(400).json({
+      message: 'This email is not registered.',
+    });
     return;
   }
   const passwordMatch = await bcrypt.compare(req.body.password, promoter.passwordHash);
   if (!passwordMatch) {
-    res.status(401);
-    res.send('There was a problem logging you in.');
+    res.status(401).json({
+      message: 'There was a problem logging you in.',
+    });
     return;
   }
   const token = jwt.sign({ ...promoter, passwordHash: '' }, process.env.WEB_TOKEN_SECRET);
