28a40547b3c8309f9f1e8afdb893d5eba6480b80
Chance Hudson <jchancehud@gmail.com>
2019-04-08 18:29:42 -0500

    Refactor auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,34 +1,32 @@
 const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 
 module.exports = (req, res, next) => {
-  const token = req.body.token || req.query.token
-  if (!token) {
+  const promoter = loadPromoter(req, res)
+  if (!promoter) {
     res.status(401)
     res.send('No authentication token supplied in body or query.')
     return
   }
-
-  try {
-    req.promoter = jwt.verify(token, process.env.WEB_TOKEN_SECRET)
-    next()
-  } catch (err) {
-    console.log('Error decoding token', err)
-    res.status(500)
-    res.send(err.toString())
-  }
+  req.promoter = promoter
+  next()
 }
 
 module.exports.notRequired = (req, res, next) => {
-  const token = req.body.token || req.query.token
-  if (!token) {
-    req.promoter = {}
-    next()
-    return
-  }
+  const promoter = loadPromoter(req, res)
+  req.promoter = promoter || {}
+  next()
+}
 
+const loadPromoter = (req, res) => {
+  const token = req.body.token || req.query.token
+  if (!token) return
   try {
-    req.promoter = jwt.verify(token, process.env.WEB_TOKEN_SECRET)
-    next()
+    const promoter = jwt.verify(token, process.env.WEB_TOKEN_SECRET)
+    if (promoter._id) {
+      promoter._id = mongoose.Types.ObjectId(promoter._id)
+    }
+    return promoter
   } catch (err) {
     console.log('Error decoding token', err)
     res.status(500)
