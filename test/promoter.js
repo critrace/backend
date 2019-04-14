0f3e09f08c0a0080c9888114ed2527c15c284431
Chance Hudson <jchancehud@gmail.com>
2019-04-14 12:17:23 -0500

    Add login test and duplicate creation test

--- test/promoter.js
+++ test/promoter.js
@@ -2,12 +2,38 @@ import test from 'ava'
 import supertest from 'supertest'
 import app from '..'
 
-test('should create promoter', async (t) => {
+const TEST_EMAIL = 'test@email.com'
+const TEST_PASSWORD = 'password'
+
+test.serial('should create promoter', async (t) => {
   await supertest(app)
     .post('/promoters')
     .send({
-      email: 'test@email.com',
-      password: 'password',
+      email: TEST_EMAIL,
+      password: TEST_PASSWORD,
     })
+    .expect(200)
+  t.pass()
+})
+
+test.serial('should fail to create duplicate email', async (t) => {
+  await supertest(app)
+    .post('/promoters')
+    .send({
+      email: TEST_EMAIL,
+      password: TEST_PASSWORD,
+    })
+    .expect(400)
+  t.pass()
+})
+
+test.serial('should login with promoter', async (t) => {
+  await supertest(app)
+    .post('/promoters/login')
+    .send({
+      email: TEST_EMAIL,
+      password: TEST_PASSWORD,
+    })
+    .expect(200)
   t.pass()
 })
