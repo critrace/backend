7453a2dc97e3bbe0000e44114172301fa70fdf4f
Chance Hudson <jchancehud@gmail.com>
2019-04-18 01:13:37 -0500

    Refactor race tests, improve coverage

--- test/race.js
+++ test/race.js
@@ -3,92 +3,71 @@ import supertest from 'supertest'
 import app from '..'
 import nanoid from 'nanoid'
 import moment from 'moment'
+import {
+  createPromoter,
+  createSeries,
+  createEvent,
+  createRace,
+  createRider,
+  createBib,
+} from './api'
+import randomObjectId from 'random-objectid'
 
 test.before(async (t) => {
-  const { body: promoter } = await supertest(app)
-    .post('/promoters')
-    .send({
-      email: `${nanoid()}@email.com`,
-      password: 'password',
-    })
-    .expect(200)
-  const { body: series } = await supertest(app)
-    .post('/series')
-    .send({
-      token: promoter.token,
-      name: 'A Test Series',
-    })
-    .expect(200)
-  const { body: event } = await supertest(app)
-    .post('/events')
-    .send({
-      token: promoter.token,
-      name: 'A test event',
-      startDate: new Date(),
-      seriesId: series._id,
-    })
-    .expect(200)
-  const { body: race } = await supertest(app)
-    .post('/races')
-    .send({
-      token: promoter.token,
-      name: 'A Test Race',
-      eventId: event._id,
-      scheduledStartTime: '15:00',
-    })
-    .expect(200)
-  const { body: rider } = await supertest(app)
-    .post('/riders')
-    .send({
-      token: promoter.token,
-      license: nanoid(),
-      licenseExpirationDate: moment().add(1, 'year'),
-      firstname: 'will',
-      lastname: 'guy',
-    })
-    .expect(200)
-  Object.assign(t.context, {
-    promoter,
-    series,
-    event,
-    race,
-    rider,
-  })
+  const { body: promoter } = await createPromoter()
+  t.context.promoter = promoter
+})
+
+test('should fail to create with bad event id', async (t) => {
+  const { token } = t.context.promoter
+  await createRace(token, {
+    eventId: await randomObjectId(),
+  }).expect(404)
+  t.pass()
 })
 
 test('should fail to start if not promoter', async (t) => {
-  const { body: promoter } = await supertest(app)
-    .post('/promoters')
-    .send({
-      email: `${nanoid()}@email.com`,
-      password: 'password',
-    })
-    .expect(200)
+  const { token } = t.context.promoter
+  const { body: promoter } = await createPromoter()
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
   await supertest(app)
     .post('/races/start')
     .send({
       token: promoter.token,
-      _id: t.context.race._id,
+      _id: race._id,
     })
     .expect(401)
   t.pass()
 })
 
 test('should start race', async (t) => {
+  const { token } = t.context.promoter
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
   await supertest(app)
     .post('/races/start')
     .send({
-      token: t.context.promoter.token,
-      _id: t.context.race._id,
+      token,
+      _id: race._id,
       actualStart: new Date(),
     })
-    .expect(204)
   // Should fail to start twice
   await supertest(app)
     .post('/races/start')
     .send({
-      token: t.context.promoter.token,
-      _id: t.context.race._id,
+      token,
+      _id: race._id,
       actualStart: new Date(),
     })
     .expect(400)
@@ -96,203 +75,356 @@ test('should start race', async (t) => {
 })
 
 test('should get entries', async (t) => {
+  const { token } = t.context.promoter
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
   await supertest(app)
     .get('/races/entries')
     .query({
-      raceId: t.context.race._id,
+      raceId: race._id,
     })
-    .expect(200)
   t.pass()
 })
 
 test('should get races if not authed', async (t) => {
-  await supertest(app)
-    .get('/races')
-    .expect(200)
+  await supertest(app).get('/races')
   t.pass()
 })
 
 test('should get races', async (t) => {
+  const { token } = t.context.promoter
   await supertest(app)
     .get('/races')
     .query({
-      token: t.context.promoter.token,
+      token,
     })
-    .expect(200)
   t.pass()
 })
 
 test('should get race by id', async (t) => {
-  const { body: race } = await supertest(app)
+  const { token } = t.context.promoter
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
+  const { body: loadedRace } = await supertest(app)
     .get('/races')
     .query({
-      _id: t.context.race._id,
+      _id: race._id,
     })
-    .expect(200)
-  t.true(race._id === t.context.race._id)
+  t.true(loadedRace._id === race._id)
   t.pass()
 })
 
 test('should get races by event', async (t) => {
+  const { token } = t.context.promoter
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
   const { body: races } = await supertest(app)
     .get('/races')
     .query({
-      eventId: t.context.event._id,
+      eventId: event._id,
     })
-    .expect(200)
-  t.truthy(races.find((race) => race._id === t.context.race._id))
+  t.truthy(races.find((_race) => _race._id === race._id))
   t.pass()
 })
 
 test('should create entry', async (t) => {
-  const { body: bib } = await supertest(app)
-    .post('/bibs')
-    .send({
-      token: t.context.promoter.token,
-      seriesId: t.context.series._id,
-      riderId: t.context.rider._id,
-      bibNumber: 5000,
-    })
-    .expect(200)
+  const { token } = t.context.promoter
+  const { body: rider } = await createRider(token)
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
+  const { body: bib } = await createBib(token, {
+    seriesId: series._id,
+    riderId: rider._id,
+  })
   await supertest(app)
     .post('/races/entry')
     .send({
-      token: t.context.promoter.token,
-      riderId: t.context.rider._id,
-      raceId: t.context.race._id,
+      token,
+      riderId: rider._id,
+      raceId: race._id,
       bibId: bib._id,
     })
-    .expect(200)
+  t.pass()
+})
+
+test('should fail to create duplicate entry', async (t) => {
+  const { token } = t.context.promoter
+  const { body: rider } = await createRider(token)
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
+  const { body: bib } = await createBib(token, {
+    seriesId: series._id,
+    riderId: rider._id,
+  })
+  await supertest(app)
+    .post('/races/entry')
+    .send({
+      token,
+      riderId: rider._id,
+      raceId: race._id,
+      bibId: bib._id,
+    })
+  await supertest(app)
+    .post('/races/entry')
+    .send({
+      token,
+      riderId: rider._id,
+      raceId: race._id,
+      bibId: bib._id,
+    })
+    .expect(400)
+  t.pass()
+})
+
+test('should fail to create entry for mismatched bib', async (t) => {
+  const { token } = t.context.promoter
+  const { body: rider } = await createRider(token)
+  const { body: rider2 } = await createRider(token)
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
+  const { body: bib } = await createBib(token, {
+    seriesId: series._id,
+    riderId: rider._id,
+  })
+  await supertest(app)
+    .post('/races/entry')
+    .send({
+      token,
+      riderId: rider2._id,
+      raceId: race._id,
+      bibId: bib._id,
+    })
+    .expect(401)
+  t.pass()
+})
+
+test('should fail to create entry for invalid bib', async (t) => {
+  const { token } = t.context.promoter
+  const { body: rider } = await createRider(token)
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
+  await supertest(app)
+    .post('/races/entry')
+    .send({
+      token,
+      riderId: rider._id,
+      raceId: race._id,
+      bibId: await randomObjectId(),
+    })
+    .expect(400)
   t.pass()
 })
 
 test('should delete entry', async (t) => {
-  const { body: rider } = await supertest(app)
-    .post('/riders')
-    .send({
-      token: t.context.promoter.token,
-      license: nanoid(),
-      licenseExpirationDate: moment().add(1, 'year'),
-      firstname: 'will',
-      lastname: 'guy',
-    })
-    .expect(200)
-  const { body: bib } = await supertest(app)
-    .post('/bibs')
-    .send({
-      token: t.context.promoter.token,
-      seriesId: t.context.series._id,
-      riderId: rider._id,
-      bibNumber: 5001,
-    })
-    .expect(200)
+  const { token } = t.context.promoter
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
+  const { body: rider } = await createRider(token)
+  const { body: bib } = await createBib(token, {
+    seriesId: series._id,
+    riderId: rider._id,
+  })
   await supertest(app)
     .post('/races/entry')
     .send({
-      token: t.context.promoter.token,
-      raceId: t.context.race._id,
+      token,
+      raceId: race._id,
       riderId: rider._id,
       bibId: bib._id,
     })
-    .expect(200)
   await supertest(app)
     .delete('/races/entries')
     .send({
-      token: t.context.promoter.token,
-      raceId: t.context.race._id,
+      token,
+      raceId: race._id,
       riderId: rider._id,
     })
-    .expect(204)
+  t.pass()
+})
+
+test('should fail to delete entry if not promoter', async (t) => {
+  const { token } = t.context.promoter
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
+  const { body: rider } = await createRider(token)
+  const { body: bib } = await createBib(token, {
+    seriesId: series._id,
+    riderId: rider._id,
+  })
+  const { body: promoter } = await createPromoter()
+  await supertest(app)
+    .post('/races/entry')
+    .send({
+      token,
+      raceId: race._id,
+      riderId: rider._id,
+      bibId: bib._id,
+    })
+  await supertest(app)
+    .delete('/races/entries')
+    .send({
+      token: promoter.token,
+      raceId: race._id,
+      riderId: rider._id,
+    })
+    .expect(401)
   t.pass()
 })
 
 test('should fail to create entry if not promoter', async (t) => {
-  const { body: promoter } = await supertest(app)
-    .post('/promoters')
-    .send({
-      email: `${nanoid()}@email.com`,
-      password: 'password',
-    })
-    .expect(200)
+  const { token } = t.context.promoter
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
+  const { body: promoter } = await createPromoter()
   await supertest(app)
     .post('/races/entry')
     .send({
       token: promoter.token,
-      raceId: t.context.race._id,
+      raceId: race._id,
     })
     .expect(401)
   t.pass()
 })
 
 test('should get leaderboard', async (t) => {
+  const { token } = t.context.promoter
+  const { body: rider } = await createRider(token)
   const transponder = nanoid()
-  const { body: rider } = await supertest(app)
-    .post('/riders')
-    .send({
-      token: t.context.promoter.token,
-      license: nanoid(),
-      licenseExpirationDate: moment().add(1, 'year'),
-      firstname: 'keith',
-      lastname: 'noot',
-      transponder,
-    })
-    .expect(200)
-  const { body: bib } = await supertest(app)
-    .post('/bibs')
-    .send({
-      token: t.context.promoter.token,
-      seriesId: t.context.series._id,
-      riderId: rider._id,
-      bibNumber: 5001,
-    })
-    .expect(200)
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
+  const { body: bib } = await createBib(token, {
+    seriesId: series._id,
+    riderId: rider._id,
+  })
   await supertest(app)
     .post('/races/entry')
     .send({
-      token: t.context.promoter.token,
+      token,
       riderId: rider._id,
-      raceId: t.context.race._id,
+      raceId: race._id,
       bibId: bib._id,
     })
-    .expect(200)
   // Add 5 laps
   for (let x = 0; x < 5; x += 1) {
     await supertest(app)
       .post('/passings')
       .send({
-        token: t.context.promoter.token,
-        raceId: t.context.race._id,
+        token,
+        raceId: race._id,
         transponder,
-        riderId: rider._id,
+        riderId: x % 2 ? rider._id : undefined,
         date: moment().add(x, 'minutes'),
       })
-      .expect(204)
   }
   await supertest(app)
     .get('/races/leaderboard')
     .query({
-      raceId: t.context.race._id,
+      raceId: race._id,
     })
-    .expect(200)
   t.pass()
 })
 
 test('should delete a race', async (t) => {
-  const { body: race } = await supertest(app)
-    .post('/races')
-    .send({
-      token: t.context.promoter.token,
-      name: 'A Test Race',
-      eventId: t.context.event._id,
-      scheduledStartTime: '15:00',
-    })
-    .expect(200)
+  const { token } = t.context.promoter
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
   await supertest(app)
     .delete('/races')
     .send({
-      token: t.context.promoter.token,
+      token,
       _id: race._id,
     })
-    .expect(204)
+  t.pass()
+})
+
+test('should fail to delete non-existant race', async (t) => {
+  const { token } = t.context.promoter
+  await supertest(app)
+    .delete('/races')
+    .send({
+      token,
+      _id: await randomObjectId(),
+    })
+    .expect(404)
+  t.pass()
+})
+
+test('should fail to delete race if not promoter', async (t) => {
+  const { token } = t.context.promoter
+  const { body: series } = await createSeries(token)
+  const { body: event } = await createEvent(token, {
+    seriesId: series._id,
+  })
+  const { body: race } = await createRace(token, {
+    eventId: event._id,
+  })
+  const { body: promoter } = await createPromoter()
+  await supertest(app)
+    .delete('/races')
+    .send({
+      token: promoter.token,
+      _id: race._id,
+    })
+    .expect(401)
   t.pass()
 })
