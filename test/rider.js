74f08ec4894bda11503676ab335b9b6861189736
Chance Hudson <jchancehud@gmail.com>
2019-04-18 00:54:31 -0500

    Refactor rider tests, improve coverage

--- test/rider.js
+++ test/rider.js
@@ -2,95 +2,92 @@ import test from 'ava'
 import supertest from 'supertest'
 import app from '..'
 import nanoid from 'nanoid'
-import moment from 'moment'
+import { createPromoter, createRider } from './api'
+import randomObjectId from 'random-objectid'
 
 test.before(async (t) => {
-  const { body: promoter } = await supertest(app)
-    .post('/promoters')
-    .send({
-      email: `${nanoid()}@email.com`,
-      password: 'password',
-    })
-    .expect(200)
-  const { body: rider } = await supertest(app)
-    .post('/riders')
-    .send({
-      token: promoter.token,
-      license: nanoid(),
-      licenseExpirationDate: moment().add(1, 'year'),
-      firstname: 'john',
-      lastname: 'doe',
-    })
-    .expect(200)
-  Object.assign(t.context, {
-    promoter,
-    rider,
-  })
+  const { body: promoter } = await createPromoter()
+  t.context.promoter = promoter
 })
 
 test('should create one day rider', async (t) => {
-  await supertest(app)
-    .post('/riders')
-    .send({
-      token: t.context.promoter.token,
-      firstname: 'tom',
-      lastname: 'willaby',
-    })
-    .expect(200)
+  const { token } = t.context.promoter
+  await createRider(token)
   t.pass()
 })
 
 test('should search for rider', async (t) => {
+  const { token } = t.context.promoter
+  const { body: rider } = await createRider(token)
   const { body } = await supertest(app)
     .get('/riders/search')
     .query({
-      search: 'jo',
+      search: rider.firstname,
     })
-    .expect(200)
   t.true(body.length !== 0)
   t.pass()
 })
 
 test('should load rider', async (t) => {
-  await supertest(app)
-    .get('/riders')
-    .expect(200)
-  await supertest(app)
-    .get('/riders')
-    .query({
-      _id: t.context.rider._id,
-    })
-    .expect(200)
+  const { token } = t.context.promoter
+  const { body: rider } = await createRider(token)
+  await supertest(app).get('/riders')
   await supertest(app)
     .get('/riders')
     .query({
-      license: t.context.rider.license,
+      _id: rider._id,
     })
-    .expect(200)
+  await supertest(app)
+    .get('/riders')
+    .query({
+      license: rider.license,
+    })
+  t.pass()
+})
+
+test('should fail to load rider with bad license', async (t) => {
+  await supertest(app)
+    .get('/riders')
+    .query({
+      license: nanoid(),
+    })
+    .expect(404)
+  t.pass()
+})
+
+test('should fail to load rider with bad _id', async (t) => {
+  await supertest(app)
+    .get('/riders')
+    .query({
+      _id: await randomObjectId(),
+    })
+    .expect(404)
   t.pass()
 })
 
 test('should update rider', async (t) => {
+  const { token } = t.context.promoter
+  const { body: rider } = await createRider(token)
   await supertest(app)
     .put('/riders')
     .send({
-      token: t.context.promoter.token,
+      token,
       where: {
-        _id: t.context.rider._id,
+        _id: rider._id,
       },
       changes: {
         firstname: 'jonathan',
       },
     })
-    .expect(204)
   t.pass()
 })
 
 test('should fail to update rider with no where', async (t) => {
+  const { token } = t.context.promoter
   await supertest(app)
     .put('/riders')
     .send({
-      token: t.context.promoter.token,
+      token,
       changes: {
         firstname: 'jonathan',
       },
@@ -100,11 +97,13 @@ test('should fail to update rider with no where', async (t) => {
 })
 
 test('should fail to update rider if not authenticated', async (t) => {
+  const { token } = t.context.promoter
+  const { body: rider } = await createRider(token)
   await supertest(app)
     .put('/riders')
     .send({
       where: {
-        _id: t.context.rider._id,
+        _id: rider._id,
       },
       changes: {
         firstname: 'jonathan',
@@ -115,13 +114,23 @@ test('should fail to update rider if not authenticated', async (t) => {
 })
 
 test('should load multiple riders', async (t) => {
+  const { token } = t.context.promoter
+  const { body: rider } = await createRider(token)
   const { body: riders } = await supertest(app)
     .post('/riders/byId')
     .send({
-      _ids: [t.context.rider._id],
+      _ids: [rider._id],
     })
-    .expect(200)
-  t.true(riders[0]._id === t.context.rider._id)
+  t.true(riders[0]._id === rider._id)
   t.true(riders.length === 1)
+  t.pass()
+})
+
+test('should return empty array', async (t) => {
+  await supertest(app)
+    .post('/riders/byId')
+    .send({
+      _ids: [],
+    })
   t.pass()
 })
