dc883e8622264937613b52c7d6951ec14896e285
Chance Hudson <jchancehud@gmail.com>
2019-04-24 19:20:07 -0500

    Add branch coverage for auth middleware

--- test/auth.js
+++ test/auth.js
@@ -2,6 +2,8 @@ import test from 'ava'
 import app from '..'
 import nanoid from 'nanoid'
 import supertest from 'supertest'
+import { createPromoter } from './api'
+import jwt from 'jsonwebtoken'
 
 test('should fail to decode invalid token', async (t) => {
   await supertest(app)
@@ -10,5 +12,17 @@ test('should fail to decode invalid token', async (t) => {
       token: nanoid(),
     })
     .expect(500)
+  t.pass()
+})
+
+// Branch logic test in auth middleware
+test('should not wrap non-existent _id', async (t) => {
+  const { body: promoter } = await createPromoter()
+  const token = jwt.sign({ ...promoter, _id: '' }, process.env.WEB_TOKEN_SECRET)
+  await supertest(app)
+    .get('/promoters')
+    .query({
+      token,
+    })
   t.pass()
 })
